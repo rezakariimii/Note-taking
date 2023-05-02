8bcbaa8f733484dc4fbe0510f600e4a8bd459752
Reza karimi <rezakariimii1997@gmail.com>
2023-05-02 10:14:52 +0330

    clean notelist.controller code

--- src/notelist/notelist.controller.js
+++ src/notelist/notelist.controller.js
@@ -2,74 +2,69 @@
   "use strict";
   
   angular.module("app").controller("TodoCtrl", NoteListController);
+
   NoteListController.$inject = ["$scope", "$http"];
 
   function NoteListController($scope, $http) {
     $scope.todos = [];
+    const pathUrl = "https://jsonplaceholder.typicode.com/posts"
 
-    $http
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then(function (response) {
-        $scope.todos = response.data.map(function (item) {
-          item.showDescription = false;
-          item.editing = false;
-          return item;
-        });
-      });
-
-    $scope.addTodo = function () {
-      $http
-        .post("https://jsonplaceholder.typicode.com/posts", {
-          title: $scope.newTodoTitle,
-          body: $scope.newTodoBody,
-          completed: false,
-        })
+    function initializeTodos() {
+      $http.get(pathUrl)
         .then(function (response) {
-          const newTodo = {
-            id: response.data.id,
-            userId: response.data.userId,
-            title: response.data.title,
-            body: response.data.body,
-            showDescription: false,
-            editing: false,
-          };
-          $scope.todos.push(newTodo);
-          $scope.newTodoTitle = "";
-          $scope.newTodoBody = "";
+          $scope.todos = response.data.map(function (item) {
+            item.showDescription = false;
+            item.editing = false;
+            return item;
+          });
         });
-    };
+    }
 
-    $scope.removeTodo = function (todo) {
-      $http
-        .delete("https://jsonplaceholder.typicode.com/posts/" + todo.id)
+    function addTodo() {
+      $http.post(pathUrl, {
+        title: $scope.newTodoTitle,
+        body: $scope.newTodoBody,
+        completed: false,
+      })
+      .then(function (response) {
+        const newTodo = {
+          id: response.data.id,
+          userId: response.data.userId,
+          title: response.data.title,
+          body: response.data.body,
+          showDescription: false,
+          editing: false,
+        };
+        $scope.todos.push(newTodo);
+        $scope.newTodoTitle = "";
+        $scope.newTodoBody = "";
+      });
+    }
+
+    function removeTodo(todo) {
+      $http.delete(`https://jsonplaceholder.typicode.com/posts/${todo.id}`)
         .then(function (response) {
           const index = $scope.todos.indexOf(todo);
           $scope.todos.splice(index, 1);
         });
-    };
+    }
 
-    $scope.editTodo = function (todo) {
+    function editTodo(todo) {
       $scope.editing = true;
       $scope.editingTodo = angular.copy(todo);
       $scope.editingTodo.editing = true;
-    };
+    }
 
-    $scope.cancelEditing = function (todo) {
+    function cancelEditing(todo) {
       $scope.editing = false;
       $scope.editingTodo = null;
       todo.editing = false;
-    };
+    }
 
-    $scope.updateTodo = function () {
-      $http
-        .put(
-          "https://jsonplaceholder.typicode.com/posts/" + $scope.editingTodo.id,
-          $scope.editingTodo
-        )
+    function updateTodo() {
+      $http.put(`https://jsonplaceholder.typicode.com/posts/${$scope.editingTodo.id}`, $scope.editingTodo)
         .then(function (response) {
-          const index = $scope.todos.findIndex(
-            (todo) => todo.id === response.data.id
-          );
+          const index = $scope.todos.findIndex((todo) => todo.id === response.data.id);
           const updatedTodo = {
             id: response.data.id,
             userId: response.data.userId,
@@ -82,10 +77,21 @@
           $scope.editing = false;
           $scope.editingTodo = null;
         });
-    };
+    }
 
-    $scope.toggleDescription = function (todo) {
+    function toggleDescription(todo) {
       todo.showDescription = !todo.showDescription;
-    };
+    }
+
+    // Initialize the todos when the controller is loaded
+    initializeTodos();
+
+    // Add functions to the scope
+    $scope.addTodo = addTodo;
+    $scope.removeTodo = removeTodo;
+    $scope.editTodo = editTodo;
+    $scope.cancelEditing = cancelEditing;
+    $scope.updateTodo = updateTodo;
+    $scope.toggleDescription = toggleDescription;
   }
 })();
